8626231622c56ad56d514b2ed0dd3270803118e9
Joachim Rosseel <jrosseel@ptb-09004559.bordeaux.inria.fr>
2025-12-11 16:22:59 +0100

    change duration of node conso

--- RX_energy_test_polar_nodes.py
+++ RX_energy_test_polar_nodes.py
@@ -69,7 +69,7 @@ proc.stdin.write(line_com.encode())
 proc.stdin.flush()
 
 # Duration of energy measurement (in seconds)
-te = 1000
+te = 450
 
 # Setting writing path for energy consumption
 write_path = "/scratch/rosseelj/energy/" + "energy_polar_" + str(N) + "_" + str(enc_info_bits) + "_CRC_" + crc_poly + "_Decoder_polar_" + dec
