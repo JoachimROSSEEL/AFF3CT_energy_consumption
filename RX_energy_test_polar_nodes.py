5bffe0db7d5b2eb4bd24ed67951cac8f85243e29
Joachim Rosseel <jrosseel@ptb-09004559.bordeaux.inria.fr>
2025-12-12 12:21:31 +0100

    Create 2 processes instead of one, one for node-conso, the other for RX chain

--- RX_energy_test_polar_nodes.py
+++ RX_energy_test_polar_nodes.py
@@ -2,6 +2,7 @@
 import itertools
 import time
 import subprocess
+# import asyncio
 
 # Noise parameters
 # Noise for FER = 1e-2
@@ -58,15 +59,15 @@ for L in range(len(polar_nodes) + 1):
 list_combi.remove('{}')
 
 # Source profile for module load
-proc = subprocess.Popen(["/bin/bash"], stdin=subprocess.PIPE, shell=True)
-line_com = "source /etc/profile\n"
-proc.stdin.write(line_com.encode())
-proc.stdin.flush()
+# proc = subprocess.Popen(["/bin/bash"], stdin=subprocess.PIPE, shell=True)
+# line_com = "source /etc/profile\n"
+# proc.stdin.write(line_com.encode())
+# proc.stdin.flush()
 
 # Load node conso 
-line_com = "module load /mnt/nfs/software/modules-env/etc/modulefiles/tools/node-conso/g8b77353\n"
-proc.stdin.write(line_com.encode())
-proc.stdin.flush()
+# line_com = "module load /mnt/nfs/software/modules-env/etc/modulefiles/tools/node-conso/g8b77353\n"
+# proc.stdin.write(line_com.encode())
+# proc.stdin.flush()
 
 # Duration of energy measurement (in seconds)
 te = 450
@@ -75,39 +76,47 @@ te = 450
 write_path = "/scratch/rosseelj/energy/" + "energy_polar_" + str(N) + "_" + str(enc_info_bits) + "_CRC_" + crc_poly + "_Decoder_polar_" + dec
 
 # Create path 
-line_com = f"mkdir -p {write_path}\n"
-proc.stdin.write(line_com.encode())
-proc.stdin.flush()
+# line_com = f"mkdir -p {write_path}\n"
+# proc.stdin.write(line_com.encode())
+# proc.stdin.flush()
 
 # Launching RX chain and energy measurement for each polar node configuration
 line_com = f""
 if __name__ == "__main__":
   for i in range(0, 1):
+    # Process for node-conso 
+    p1 = subprocess.Popen(["/bin/bash"], stdin=subprocess.PIPE, shell=True)
+    # Loading node-conso 
+    line_com = "source /etc/profile\n"
+    p1.stdin.write(line_com.encode())
+    p1.stdin.flush()
+
     # Starting the  board
     line_com = "node-conso -P 1\n"
-    proc.stdin.write(line_com.encode())
-    proc.stdin.flush()
+    p1.stdin.write(line_com.encode())
+    p1.stdin.flush()
 
     # Clearing registers
     line_com = "node-conso -m 1\n"
-    proc.stdin.write(line_com.encode())
-    proc.stdin.flush()
+    p1.stdin.write(line_com.encode())
+    p1.stdin.flush()
 
     # Lauching node consommation measurement
     write_file = write_path + "/Decoder_polar_" + dec + "_nodes_" + list_combi[i] + ".txt"
-    line_com = f"node-conso -M 1 -t {te} > {write_file} &\n"
-    proc.stdin.write(line_com.encode())
-    proc.stdin.flush()
+    line_com = f"node-conso -M 1 -t {te} > {write_file} \n"
+    p1.stdin.write(line_com.encode())
+    p1.stdin.flush()
 
     time.sleep(5)
 
     # Launching RX chain 
+    p2 = subprocess.Popen(["/bin/bash"], stdin=subprocess.PIPE, shell=True)
     line_com = f"./RX_energy_test --crc-poly {crc_poly} --crc-info-bits {crc_info_bits} --crc-size {crc_size} --enc-info-bits {enc_info_bits} -N {N} --enc-fb-noise {enc_fb_noise} --mnt-info-bits {crc_info_bits} -n {n} -m {Eb_N0_min} -M {Eb_N0_max} -s {step} -D {dec} --dec-implem {dec_implem} --dec-polar-nodes {list_combi[i]}\n"
     subprocess.Popen([line_com], shell = True)
-    proc.stdin.write(line_com.encode())
-    proc.stdin.flush() 
-    proc.communicate()
-    proc.wait()
+    p2.stdin.write(line_com.encode())
+    p2.stdin.flush() 
+    p1.wait()
+    p2.wait()
     
     time.sleep(30)
 
