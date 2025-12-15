f677d9fa3822e36b1e518f8604491dc58d86ffca
Joachim Rosseel <jrosseel@ptb-09004559.bordeaux.inria.fr>
2025-12-15 11:43:17 +0100

    Create 2 processes instead of one, one for node-conso, the other for RX chain

--- RX_energy_test_Dalek.sh
+++ RX_energy_test_Dalek.sh
@@ -18,9 +18,21 @@ node-conso -M 1 -t $1 > $2 &
 
 sleep 1
 echo $1
+echo $2
+echo $3
+echo $4
+echo $5
+echo $6
 echo $7
+echo $8
+echo $9
+echo $10
 echo $11
+echo $12
+echo $13
 echo $14
+echo $15
+echo $16
 echo "Running RX chain with energy test."
 ./RX_energy_test \
 --crc-poly $3 \
