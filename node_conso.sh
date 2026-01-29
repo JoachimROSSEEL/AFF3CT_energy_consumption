0d020deb7148f9a0a23448b090bdbdd1d04bc7a0
Joachim Rosseel <jrosseel@ptb-09004559.bordeaux.inria.fr>
2026-01-29 10:50:20 +0100

    Added command for measure with 2nd probe (GPU)

--- node_conso.sh
+++ node_conso.sh
@@ -18,6 +18,18 @@
 # # Apply CPU configuration from yaml file
 # cpudev apply --config config_cpu_az4.yaml
 
+
+# Load module cpudev : enables to modify the CPU driver, its governor, its frequency per core, idle states, and so on
+# module load cpudev
+# echo $(pwd)
+# echo $(cpudev apply --help)
+# Apply CPU configuration from yaml file
+# cpudev cpufreq --config config_cpu_az4.yaml
+# cpudev driver acpi
+# cpudev cpufreq --governor userspace --policies "0-3" --frequency "2500000"
+# cpudev cpufreq --governor rosseelj --policies "0-3"
+
+
 sleep 1
 
 module load ncm/gdcc873f
@@ -28,5 +40,6 @@ node-conso -P 1 # <= turn on I2C chain 1
 node-conso -P 2 # <= turn on I2C chain 2
 node-conso -m 1
 node-conso -m 2
-node-conso -M 1 -t 30 > /scratch/rosseelj/conso/conso_test.txt
+node-conso -M 1 
 node-conso -M 2
+node-conso -t 30 > /scratch/rosseelj/conso/conso_test.txt
