3a7d44d52ec55b850dad1ea9689fd0955cbe7f15
Joachim Rosseel <jrosseel@ptb-09004559.bordeaux.inria.fr>
2026-02-18 13:11:24 +0100

    added run time measurement and write its results on runtime file

--- main_RX_energy_test_Dalek.cpp
+++ main_RX_energy_test_Dalek.cpp
@@ -191,7 +191,7 @@ int main(int argc, char** argv, char** env) {
     ///////////////////////////////////
     // Simulation execution
     
-    int nb_repeat = 10; 
+    int nb_repeat = 1; 
     for (int k = 0; k < nb_repeat; k++)
     {
         for (size_t i = 0; i < monitor_factory.max_frame; i++){
@@ -224,11 +224,13 @@ int main(int argc, char** argv, char** env) {
     // File to write execution time
     std::string dec_name(argv[argc-3]);
     std::string nodes(argv[argc-1]);
+    std::cout << dec_name << "\n";
+    std::cout << nodes << "\n";
     std::string filepath_runtime = "/scratch/rosseelj/runtime/runtime_RX_" + std::to_string(N) + "_" + std::to_string(encoder.get_K()) + "_CRC_" + CRC_factory.type + "_Decoder_polar_" + dec_name;
     // argc-3 : Decoder and argc-1 : node configuation
     std::string filename_runtime = "/Decoder_polar_" + dec_name + "_nodes_" + nodes + ".txt";
     std::filesystem::create_directory(filepath_runtime);
-    filename = filepath_runtime + filename_runtime;
+    filename_runtime = filepath_runtime + filename_runtime;
     std::ofstream filestream_runtime;
     filestream_runtime.open(filename_runtime);
     filestream_runtime << "Node_configuration Run_Time(s) \n";
